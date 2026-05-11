ce72fa2b17d5101c6280bffc2bcda63a6264e2aa
CHANGEME <CHANGEME>
2026-05-11 06:13:22 -0300

    feat: reason with realtime buffers and logs on files

--- clean_narseses.sh
+++ clean_narseses.sh
@@ -1,15 +1,30 @@
 #!/usr/bin/env sh
 
-sed 's/`//g' |
-    sed 's/`//g' |
-    sed 's/```//g' |
-    sed 's/^/</g' |
-    sed 's/$/>./g' |
-    sed 's/<</</g' |
-    sed 's/>.>./>./g' |
-    sed 's/>. </>.\n</g' |
-    sed 's/<>.//g' |
+# sed 's/`//g' |
+#     sed 's/`//g' |
+#     sed 's/```//g' |
+#     sed 's/^/</g' |
+#     sed 's/$/>./g' |
+#     sed 's/<</</g' |
+#     sed 's/>.>./>./g' |
+#     sed 's/>. </>.\n</g' |
+#     sed 's/<>.//g' |
+# 
+#     sed 's/<narsese>.//g' |
+#     grep -v '^$'
 
-    sed 's/<narsese>.//g' |
-    grep -v '^$'
-    
+
+#!/usr/bin/env sh
+# Clean Narsese with line buffering
+
+stdbuf -oL sed 's/`//g' |
+    stdbuf -oL sed 's/`//g' |
+    stdbuf -oL sed 's/```//g' |
+    stdbuf -oL sed 's/^/</g' |
+    stdbuf -oL sed 's/$/>./g' |
+    stdbuf -oL sed 's/<</</g' |
+    stdbuf -oL sed 's/>.>./>./g' |
+    stdbuf -oL sed 's/>. </>.\n</g' |
+    stdbuf -oL sed 's/<>.//g' |
+    stdbuf -oL sed 's/<narsese>.//g' |
+    stdbuf -oL grep --line-buffered -v '^$'
--- reason.sh
+++ reason.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env sh
 
+# sh semantic.sh --plain-nal $1 | sh clean_narseses.sh |
+#    ./../OpenNARS-for-Applications/NAR shell | sh extract_derived.sh |
+#    python3 narsese2json.py | uv run --with requests python send2graph.py 
 
-sh semantic.sh --plain-nal $1 |  sh clean_narseses.sh | ../OpenNARS-for-Applications/NAR shell |
- sh extract_nal.sh | python3 narsese2json.py | uv run --with requests python send2graph.py
+
+touch input.nal derived.nal
+
+tail -f input.nal | python3 narsese2json.py | uv run --with requests python send2graph.py &
+
+tail -f input.nal | ./../OpenNARS-for-Applications/NAR shell >> derived.nal &
+
+tail -f derived.nal | sh extract_derived.sh | python3 narsese2json.py | uv run --with requests python send2graph.py & 
+
+  
+sh semantic.sh --plain-nal $1 |  sh clean_narseses.sh >> input.nal
