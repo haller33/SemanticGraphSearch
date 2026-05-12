f79fcd9640da570917f49e69672e0ff9fbdd439e
CHANGEME <CHANGEME>
2026-05-11 22:02:10 -0300

    feat: add some other helper script for better clean derived output of NARS

---
+++ extract_derived.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+grep '^Derived:' |
+    sed 's/^Derived: //' |
+    sed 's/\. Priority=.*/>./' |
+    grep -v '^$'
+
+# sed 's/>. Priority=.*/>./g' |
+#    head -n -18 |
+#    grep 'Derived: ' |
+#    cut -d':' -f2- |
+#    grep -v '^$'
