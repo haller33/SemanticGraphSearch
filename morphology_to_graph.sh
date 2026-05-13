e119499f192eaf14fdb025bb908cd065f1f572c8
CHANGEME <CHANGEME>
2026-05-13 19:05:35 -0300

    feat: improve command to print the narseses generated

--- morphology_to_graph.sh
+++ morphology_to_graph.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env sh
 
-MORPHLIB=morpheus-perseids/stemlib morpheus-perseids/bin/morpheus -L "$1" |
-        uv run python morphology_to_nal.py # |
-#        python3 narsese2json.v2.py --tag input # |
-#        uv run --with requests python send2graph.py
+PLAIN_NAL=0
+
+# Parse flags
+while [ "$1" = "--plain-nal" ]; do
+    PLAIN_NAL=1
+    shift
+done
+
+if [ "$PLAIN_NAL" -eq 1 ]; then
+    MORPHLIB=morpheus-perseids/stemlib morpheus-perseids/bin/morpheus -L "$1" |
+        uv run python morphology_to_nal.py
+else
+    MORPHLIB=morpheus-perseids/stemlib morpheus-perseids/bin/morpheus -L "$1" |
+        uv run python morphology_to_nal.py |
+        python3 narsese2json.v3.py --tag input |
+        uv run --with requests python send2graph.py
+fi 
